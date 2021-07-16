e5f43e1e7c797a330c3c827040e00eb8b4c037c1
Abduvali Zokhidov <80103407+Abdulvaliy@users.noreply.github.com>
2021-07-17 00:01:05 +0500

    Add files via upload

---
+++ sum of digits.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = str(two_digit_number)
+first = int(a[0])
+second = int(a[1])
+print(first + second)
+
+
+
+
